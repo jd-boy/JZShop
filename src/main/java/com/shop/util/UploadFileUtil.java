0e04b2c6119aff405ed89c1621712cf73f21d135
123 <1298462930@qq.com>
2019-05-22 15:52:01 +0800

    小修改

--- src/main/java/com/shop/util/UploadFileUtil.java
+++ src/main/java/com/shop/util/UploadFileUtil.java
@@ -2,6 +2,11 @@ package com.shop.util;
 
 public class UploadFileUtil {
 	
+	/*public static String JZSHOP_DOMSIN = "http://47.93.231.245:8080/";
+	public static String IMAGE_DIR = "/home/images/";
+	public static String HEAD_DIR = "/home/heads/";
+	public static String STATIC_DIR = "/home/static/";*/
+	
 	public static String JZSHOP_DOMSIN = "http://localhost:8080/";
 	public static String IMAGE_DIR = "D:/images/";
 	public static String HEAD_DIR = "D:/heads/";
